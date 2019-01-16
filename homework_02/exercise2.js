9ee3e3c13b94dd66677db136612af71c3f215442
duynga <loduynga@gmail.com>
2019-01-16 10:05:29 -0600

    Exercise2: Implement odd function that works asynchronously

--- homework_02/exercise2.js
+++ homework_02/exercise2.js
@@ -11,6 +11,15 @@ Array.prototype.even = function() {
 }
 
 Array.prototype.odd = function() {
+	let thisArray = this;
+	(new Promise((resolve, reject) => {
+		if(thisArray === undefined || thisArray === null) {
+			reject('null or undefined is not an array.');
+		} else {
+			resolve(thisArray.filter(item => item % 2 !== 0));
+		}
+	})).then(arr => console.log(arr))
+		.catch(err => console.log(err));
 }
 
 console.log('start');
