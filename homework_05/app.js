34fb2fc5fec40d2aaa3d87b8c74a921a6debe0da
Binh Tran <binh12121989@gmail.com>
2019-01-19 07:37:42 -0600

    Homework 5: Add pagination Link into response header; Setting Cache-Control

--- homework_05/app.js
+++ homework_05/app.js
@@ -9,6 +9,18 @@ async function getUsers(req, resp) {
 		let users = await axios.get('https://randomuser.me/api/?results=10');
 		resp.status(200);
 		resp.set('Content-Type', 'application/json');
+		//Set cache response by using Cache-Control or Expires
+		//Cache-Control private means this cache on client only, not on any intermediate servers like proxy
+		resp.set('Cache-Control', 'private max-age=3600');
+		//resp.set('Expires', 'Sat, 19 Jan 2019 14:00:00 GMT');
+		//Set Link on header for pagination
+		const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+		const pages = [`<${fullUrl}?page=1>;rel="first"`, 
+						`<${fullUrl}?page=1>;rel="prev"`,
+						`<${fullUrl}?page=1>;rel="next"`,
+						`<${fullUrl}?page=1>;rel="last"`];
+		resp.set('Link', pages.join(','));
+
 		resp.send(users.data);
 		resp.end();
 	} catch (err) {
@@ -17,6 +29,8 @@ async function getUsers(req, resp) {
 	}
 }
 
+//Forward client information (ip) to server even if the server stand behind a proxy
+app.enable('trust proxy');
 //Prevent client from knowing server framework
 app.disable('x-powered-by');
 //Route should be case sensitive and strict
